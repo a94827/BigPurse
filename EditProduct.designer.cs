9c4144a7f3f0b9b48010a98442cfe8dddb254f57
Alexander Ageyev <s94827@yandex.ru>
2022-01-21 21:28:27 +0500

    incomplete

--- EditProduct.designer.cs
+++ EditProduct.designer.cs
@@ -40,26 +40,26 @@
       this.label1 = new System.Windows.Forms.Label();
       this.tabPage2 = new System.Windows.Forms.TabPage();
       this.MainPanel2 = new System.Windows.Forms.Panel();
-      this.edUnit2List = new System.Windows.Forms.TextBox();
-      this.cbUnit2Presence = new System.Windows.Forms.ComboBox();
       this.groupBox3 = new System.Windows.Forms.GroupBox();
-      this.edUnit1List = new System.Windows.Forms.TextBox();
-      this.cbUnit1Presence = new System.Windows.Forms.ComboBox();
-      this.groupBox2 = new System.Windows.Forms.GroupBox();
-      this.cbDescriptionPresence = new System.Windows.Forms.ComboBox();
       this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
-      this.groupBox6 = new System.Windows.Forms.GroupBox();
-      this.lblInfoMU1 = new System.Windows.Forms.Label();
-      this.panel1 = new System.Windows.Forms.Panel();
-      this.panSpbMU1 = new System.Windows.Forms.Panel();
-      this.grMU1 = new System.Windows.Forms.DataGridView();
+      this.groupBox8 = new System.Windows.Forms.GroupBox();
+      this.cbUnit2Presence = new System.Windows.Forms.ComboBox();
       this.groupBox4 = new System.Windows.Forms.GroupBox();
       this.panel2 = new System.Windows.Forms.Panel();
       this.grMU2 = new System.Windows.Forms.DataGridView();
       this.panSpbMU2 = new System.Windows.Forms.Panel();
       this.lblInfoMU2 = new System.Windows.Forms.Label();
+      this.edUnit2List = new System.Windows.Forms.TextBox();
+      this.edUnit1List = new System.Windows.Forms.TextBox();
+      this.groupBox6 = new System.Windows.Forms.GroupBox();
+      this.panel1 = new System.Windows.Forms.Panel();
+      this.grMU1 = new System.Windows.Forms.DataGridView();
+      this.panSpbMU1 = new System.Windows.Forms.Panel();
+      this.lblInfoMU1 = new System.Windows.Forms.Label();
       this.groupBox7 = new System.Windows.Forms.GroupBox();
-      this.groupBox8 = new System.Windows.Forms.GroupBox();
+      this.cbUnit1Presence = new System.Windows.Forms.ComboBox();
+      this.groupBox2 = new System.Windows.Forms.GroupBox();
+      this.cbDescriptionPresence = new System.Windows.Forms.ComboBox();
       this.tabControl1.SuspendLayout();
       this.tabPage1.SuspendLayout();
       this.MainPanel1.SuspendLayout();
@@ -68,16 +68,16 @@
       this.tabPage2.SuspendLayout();
       this.MainPanel2.SuspendLayout();
       this.groupBox3.SuspendLayout();
-      this.groupBox2.SuspendLayout();
       this.tableLayoutPanel1.SuspendLayout();
-      this.groupBox6.SuspendLayout();
-      this.panel1.SuspendLayout();
-      ((System.ComponentModel.ISupportInitialize)(this.grMU1)).BeginInit();
+      this.groupBox8.SuspendLayout();
       this.groupBox4.SuspendLayout();
       this.panel2.SuspendLayout();
       ((System.ComponentModel.ISupportInitialize)(this.grMU2)).BeginInit();
+      this.groupBox6.SuspendLayout();
+      this.panel1.SuspendLayout();
+      ((System.ComponentModel.ISupportInitialize)(this.grMU1)).BeginInit();
       this.groupBox7.SuspendLayout();
-      this.groupBox8.SuspendLayout();
+      this.groupBox2.SuspendLayout();
       this.SuspendLayout();
       // 
       // tabControl1
@@ -97,7 +97,7 @@
       this.tabPage1.Location = new System.Drawing.Point(4, 22);
       this.tabPage1.Name = "tabPage1";
       this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
-      this.tabPage1.Size = new System.Drawing.Size(574, 290);
+      this.tabPage1.Size = new System.Drawing.Size(574, 336);
       this.tabPage1.TabIndex = 0;
       this.tabPage1.Text = "Общие";
       this.tabPage1.UseVisualStyleBackColor = true;
@@ -109,7 +109,7 @@
       this.MainPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
       this.MainPanel1.Location = new System.Drawing.Point(3, 3);
       this.MainPanel1.Name = "MainPanel1";
-      this.MainPanel1.Size = new System.Drawing.Size(568, 284);
+      this.MainPanel1.Size = new System.Drawing.Size(568, 330);
       this.MainPanel1.TabIndex = 0;
       // 
       // groupBox5
@@ -118,7 +118,7 @@
       this.groupBox5.Dock = System.Windows.Forms.DockStyle.Fill;
       this.groupBox5.Location = new System.Drawing.Point(0, 83);
       this.groupBox5.Name = "groupBox5";
-      this.groupBox5.Size = new System.Drawing.Size(568, 201);
+      this.groupBox5.Size = new System.Drawing.Size(568, 247);
       this.groupBox5.TabIndex = 1;
       this.groupBox5.TabStop = false;
       this.groupBox5.Text = "Комментари&й";
@@ -132,7 +132,7 @@
       this.edComment.Multiline = true;
       this.edComment.Name = "edComment";
       this.edComment.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
-      this.edComment.Size = new System.Drawing.Size(556, 176);
+      this.edComment.Size = new System.Drawing.Size(556, 222);
       this.edComment.TabIndex = 0;
       // 
       // groupBox1
@@ -206,26 +206,6 @@
       this.MainPanel2.Size = new System.Drawing.Size(568, 330);
       this.MainPanel2.TabIndex = 0;
       // 
-      // edUnit2List
-      // 
-      this.edUnit2List.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left)
-                  | System.Windows.Forms.AnchorStyles.Right)));
-      this.edUnit2List.Location = new System.Drawing.Point(284, 252);
-      this.edUnit2List.Name = "edUnit2List";
-      this.edUnit2List.Size = new System.Drawing.Size(275, 20);
-      this.edUnit2List.TabIndex = 3;
-      // 
-      // cbUnit2Presence
-      // 
-      this.cbUnit2Presence.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left)
-                  | System.Windows.Forms.AnchorStyles.Right)));
-      this.cbUnit2Presence.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
-      this.cbUnit2Presence.FormattingEnabled = true;
-      this.cbUnit2Presence.Location = new System.Drawing.Point(6, 19);
-      this.cbUnit2Presence.Name = "cbUnit2Presence";
-      this.cbUnit2Presence.Size = new System.Drawing.Size(263, 21);
-      this.cbUnit2Presence.TabIndex = 1;
-      // 
       // groupBox3
       // 
       this.groupBox3.Controls.Add(this.tableLayoutPanel1);
@@ -236,48 +216,6 @@
       this.groupBox3.TabIndex = 1;
       this.groupBox3.TabStop = false;
       this.groupBox3.Text = "Поля \"Количество\" и \"Единица измерения\"";
-      // 
-      // edUnit1List
-      // 
-      this.edUnit1List.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left)
-                  | System.Windows.Forms.AnchorStyles.Right)));
-      this.edUnit1List.Location = new System.Drawing.Point(3, 252);
-      this.edUnit1List.Name = "edUnit1List";
-      this.edUnit1List.Size = new System.Drawing.Size(275, 20);
-      this.edUnit1List.TabIndex = 3;
-      // 
-      // cbUnit1Presence
-      // 
-      this.cbUnit1Presence.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left)
-                  | System.Windows.Forms.AnchorStyles.Right)));
-      this.cbUnit1Presence.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
-      this.cbUnit1Presence.FormattingEnabled = true;
-      this.cbUnit1Presence.Location = new System.Drawing.Point(6, 19);
-      this.cbUnit1Presence.Name = "cbUnit1Presence";
-      this.cbUnit1Presence.Size = new System.Drawing.Size(263, 21);
-      this.cbUnit1Presence.TabIndex = 1;
-      // 
-      // groupBox2
-      // 
-      this.groupBox2.Controls.Add(this.cbDescriptionPresence);
-      this.groupBox2.Dock = System.Windows.Forms.DockStyle.Top;
-      this.groupBox2.Location = new System.Drawing.Point(0, 0);
-      this.groupBox2.Name = "groupBox2";
-      this.groupBox2.Size = new System.Drawing.Size(568, 42);
-      this.groupBox2.TabIndex = 0;
-      this.groupBox2.TabStop = false;
-      this.groupBox2.Text = "Наличие поля \"Описание\"";
-      // 
-      // cbDescriptionPresence
-      // 
-      this.cbDescriptionPresence.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left)
-                  | System.Windows.Forms.AnchorStyles.Right)));
-      this.cbDescriptionPresence.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
-      this.cbDescriptionPresence.FormattingEnabled = true;
-      this.cbDescriptionPresence.Location = new System.Drawing.Point(12, 14);
-      this.cbDescriptionPresence.Name = "cbDescriptionPresence";
-      this.cbDescriptionPresence.Size = new System.Drawing.Size(550, 21);
-      this.cbDescriptionPresence.TabIndex = 1;
       // 
       // tableLayoutPanel1
       // 
@@ -301,56 +239,28 @@
       this.tableLayoutPanel1.Size = new System.Drawing.Size(562, 269);
       this.tableLayoutPanel1.TabIndex = 0;
       // 
-      // groupBox6
+      // groupBox8
       // 
-      this.groupBox6.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
-                  | System.Windows.Forms.AnchorStyles.Left)
+      this.groupBox8.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left)
                   | System.Windows.Forms.AnchorStyles.Right)));
-      this.groupBox6.Controls.Add(this.panel1);
-      this.groupBox6.Controls.Add(this.lblInfoMU1);
-      this.groupBox6.Location = new System.Drawing.Point(3, 58);
-      this.groupBox6.Name = "groupBox6";
-      this.groupBox6.Size = new System.Drawing.Size(275, 188);
-      this.groupBox6.TabIndex = 2;
-      this.groupBox6.TabStop = false;
-      this.groupBox6.Text = "Фиксированный список ед.изм. 1";
+      this.groupBox8.Controls.Add(this.cbUnit2Presence);
+      this.groupBox8.Location = new System.Drawing.Point(284, 3);
+      this.groupBox8.Name = "groupBox8";
+      this.groupBox8.Size = new System.Drawing.Size(275, 49);
+      this.groupBox8.TabIndex = 7;
+      this.groupBox8.TabStop = false;
+      this.groupBox8.Text = "Наличие количества &2";
       // 
-      // lblInfoMU1
+      // cbUnit2Presence
       // 
-      this.lblInfoMU1.Dock = System.Windows.Forms.DockStyle.Bottom;
-      this.lblInfoMU1.Location = new System.Drawing.Point(3, 162);
-      this.lblInfoMU1.Name = "lblInfoMU1";
-      this.lblInfoMU1.Size = new System.Drawing.Size(269, 23);
-      this.lblInfoMU1.TabIndex = 1;
-      this.lblInfoMU1.Text = "???";
-      this.lblInfoMU1.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
-      // 
-      // panel1
-      // 
-      this.panel1.Controls.Add(this.grMU1);
-      this.panel1.Controls.Add(this.panSpbMU1);
-      this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
-      this.panel1.Location = new System.Drawing.Point(3, 16);
-      this.panel1.Name = "panel1";
-      this.panel1.Size = new System.Drawing.Size(269, 146);
-      this.panel1.TabIndex = 0;
-      // 
-      // panSpbMU1
-      // 
-      this.panSpbMU1.Dock = System.Windows.Forms.DockStyle.Top;
-      this.panSpbMU1.Location = new System.Drawing.Point(0, 0);
-      this.panSpbMU1.Name = "panSpbMU1";
-      this.panSpbMU1.Size = new System.Drawing.Size(269, 34);
-      this.panSpbMU1.TabIndex = 0;
-      // 
-      // grMU1
-      // 
-      this.grMU1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-      this.grMU1.Dock = System.Windows.Forms.DockStyle.Fill;
-      this.grMU1.Location = new System.Drawing.Point(0, 34);
-      this.grMU1.Name = "grMU1";
-      this.grMU1.Size = new System.Drawing.Size(269, 112);
-      this.grMU1.TabIndex = 1;
+      this.cbUnit2Presence.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left)
+                  | System.Windows.Forms.AnchorStyles.Right)));
+      this.cbUnit2Presence.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+      this.cbUnit2Presence.FormattingEnabled = true;
+      this.cbUnit2Presence.Location = new System.Drawing.Point(6, 19);
+      this.cbUnit2Presence.Name = "cbUnit2Presence";
+      this.cbUnit2Presence.Size = new System.Drawing.Size(263, 21);
+      this.cbUnit2Presence.TabIndex = 1;
       // 
       // groupBox4
       // 
@@ -403,6 +313,75 @@
       this.lblInfoMU2.Text = "???";
       this.lblInfoMU2.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
       // 
+      // edUnit2List
+      // 
+      this.edUnit2List.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left)
+                  | System.Windows.Forms.AnchorStyles.Right)));
+      this.edUnit2List.Location = new System.Drawing.Point(284, 252);
+      this.edUnit2List.Name = "edUnit2List";
+      this.edUnit2List.Size = new System.Drawing.Size(275, 20);
+      this.edUnit2List.TabIndex = 3;
+      // 
+      // edUnit1List
+      // 
+      this.edUnit1List.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left)
+                  | System.Windows.Forms.AnchorStyles.Right)));
+      this.edUnit1List.Location = new System.Drawing.Point(3, 252);
+      this.edUnit1List.Name = "edUnit1List";
+      this.edUnit1List.Size = new System.Drawing.Size(275, 20);
+      this.edUnit1List.TabIndex = 3;
+      // 
+      // groupBox6
+      // 
+      this.groupBox6.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
+                  | System.Windows.Forms.AnchorStyles.Left)
+                  | System.Windows.Forms.AnchorStyles.Right)));
+      this.groupBox6.Controls.Add(this.panel1);
+      this.groupBox6.Controls.Add(this.lblInfoMU1);
+      this.groupBox6.Location = new System.Drawing.Point(3, 58);
+      this.groupBox6.Name = "groupBox6";
+      this.groupBox6.Size = new System.Drawing.Size(275, 188);
+      this.groupBox6.TabIndex = 2;
+      this.groupBox6.TabStop = false;
+      this.groupBox6.Text = "Фиксированный список ед.изм. 1";
+      // 
+      // panel1
+      // 
+      this.panel1.Controls.Add(this.grMU1);
+      this.panel1.Controls.Add(this.panSpbMU1);
+      this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
+      this.panel1.Location = new System.Drawing.Point(3, 16);
+      this.panel1.Name = "panel1";
+      this.panel1.Size = new System.Drawing.Size(269, 146);
+      this.panel1.TabIndex = 0;
+      // 
+      // grMU1
+      // 
+      this.grMU1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+      this.grMU1.Dock = System.Windows.Forms.DockStyle.Fill;
+      this.grMU1.Location = new System.Drawing.Point(0, 34);
+      this.grMU1.Name = "grMU1";
+      this.grMU1.Size = new System.Drawing.Size(269, 112);
+      this.grMU1.TabIndex = 1;
+      // 
+      // panSpbMU1
+      // 
+      this.panSpbMU1.Dock = System.Windows.Forms.DockStyle.Top;
+      this.panSpbMU1.Location = new System.Drawing.Point(0, 0);
+      this.panSpbMU1.Name = "panSpbMU1";
+      this.panSpbMU1.Size = new System.Drawing.Size(269, 34);
+      this.panSpbMU1.TabIndex = 0;
+      // 
+      // lblInfoMU1
+      // 
+      this.lblInfoMU1.Dock = System.Windows.Forms.DockStyle.Bottom;
+      this.lblInfoMU1.Location = new System.Drawing.Point(3, 162);
+      this.lblInfoMU1.Name = "lblInfoMU1";
+      this.lblInfoMU1.Size = new System.Drawing.Size(269, 23);
+      this.lblInfoMU1.TabIndex = 1;
+      this.lblInfoMU1.Text = "???";
+      this.lblInfoMU1.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+      // 
       // groupBox7
       // 
       this.groupBox7.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left)
@@ -415,17 +394,38 @@
       this.groupBox7.TabStop = false;
       this.groupBox7.Text = "Наличие количества &1";
       // 
-      // groupBox8
+      // cbUnit1Presence
       // 
-      this.groupBox8.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left)
+      this.cbUnit1Presence.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left)
                   | System.Windows.Forms.AnchorStyles.Right)));
-      this.groupBox8.Controls.Add(this.cbUnit2Presence);
-      this.groupBox8.Location = new System.Drawing.Point(284, 3);
-      this.groupBox8.Name = "groupBox8";
-      this.groupBox8.Size = new System.Drawing.Size(275, 49);
-      this.groupBox8.TabIndex = 7;
-      this.groupBox8.TabStop = false;
-      this.groupBox8.Text = "Наличие количества &2";
+      this.cbUnit1Presence.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+      this.cbUnit1Presence.FormattingEnabled = true;
+      this.cbUnit1Presence.Location = new System.Drawing.Point(6, 19);
+      this.cbUnit1Presence.Name = "cbUnit1Presence";
+      this.cbUnit1Presence.Size = new System.Drawing.Size(263, 21);
+      this.cbUnit1Presence.TabIndex = 1;
+      // 
+      // groupBox2
+      // 
+      this.groupBox2.Controls.Add(this.cbDescriptionPresence);
+      this.groupBox2.Dock = System.Windows.Forms.DockStyle.Top;
+      this.groupBox2.Location = new System.Drawing.Point(0, 0);
+      this.groupBox2.Name = "groupBox2";
+      this.groupBox2.Size = new System.Drawing.Size(568, 42);
+      this.groupBox2.TabIndex = 0;
+      this.groupBox2.TabStop = false;
+      this.groupBox2.Text = "Наличие поля \"Описание\"";
+      // 
+      // cbDescriptionPresence
+      // 
+      this.cbDescriptionPresence.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left)
+                  | System.Windows.Forms.AnchorStyles.Right)));
+      this.cbDescriptionPresence.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+      this.cbDescriptionPresence.FormattingEnabled = true;
+      this.cbDescriptionPresence.Location = new System.Drawing.Point(12, 14);
+      this.cbDescriptionPresence.Name = "cbDescriptionPresence";
+      this.cbDescriptionPresence.Size = new System.Drawing.Size(550, 21);
+      this.cbDescriptionPresence.TabIndex = 1;
       // 
       // EditProduct
       // 
@@ -444,17 +444,17 @@
       this.tabPage2.ResumeLayout(false);
       this.MainPanel2.ResumeLayout(false);
       this.groupBox3.ResumeLayout(false);
-      this.groupBox2.ResumeLayout(false);
       this.tableLayoutPanel1.ResumeLayout(false);
       this.tableLayoutPanel1.PerformLayout();
-      this.groupBox6.ResumeLayout(false);
-      this.panel1.ResumeLayout(false);
-      ((System.ComponentModel.ISupportInitialize)(this.grMU1)).EndInit();
+      this.groupBox8.ResumeLayout(false);
       this.groupBox4.ResumeLayout(false);
       this.panel2.ResumeLayout(false);
       ((System.ComponentModel.ISupportInitialize)(this.grMU2)).EndInit();
+      this.groupBox6.ResumeLayout(false);
+      this.panel1.ResumeLayout(false);
+      ((System.ComponentModel.ISupportInitialize)(this.grMU1)).EndInit();
       this.groupBox7.ResumeLayout(false);
-      this.groupBox8.ResumeLayout(false);
+      this.groupBox2.ResumeLayout(false);
       this.ResumeLayout(false);
 
     }
